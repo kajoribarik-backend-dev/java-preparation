9e84505c024af1d454accf7814fca313ee32cfe7
Kajori Barik <90760324+kajoribarik-backend-dev@users.noreply.github.com>
2026-07-14 16:12:44 +0530

    implement increment and decrement operators

--- src/operators/IncreDecrimental.java
+++ src/operators/IncreDecrimental.java
@@ -3,13 +3,12 @@ package operators;
 import java.util.Scanner;
 
 /**
- * Take an integer as input. Print
- * Original value
- * Value after pre-increment
- * Value after post-increment
- * Value after pre-decrement
- * Value after post-decrement
- Concepts: ++, --
+ Concepts:
+ - Increment Operator (++)
+ - Decrement Operator (--)
+
+ Time Complexity: O(1)
+ Space Complexity: O(1)
  * */
 public class IncreDecrimental {
     public static void main(String[] args) {
@@ -17,22 +16,39 @@ public class IncreDecrimental {
         System.out.println("Enter the number");
        int number = scanner.nextInt();
         System.out.println("Original value = " +number );
-       int result = increment(number);
-      System.out.println("Value after post-increment : " +result);
-        int result1 = decrement(number);
-        System.out.println("Value after post-decrement : " +result1);
-    }
-    public static int increment(int number){
+       int preResult = preIncrement(number);
+        System.out.println("Value after pre-increment : "+preResult);
+        int postResult = postIncrement(number);
+        System.out.println("Value after post-increment : "+postResult);
+        int preDecResult = preDecrement(number);
+        System.out.println("Value after pre-decrement : " + preDecResult);
+        int postDecResult = postDecrement(number);
+        System.out.println("Value after post-decrement : " + postDecResult);
+        scanner.close();
 
-        System.out.println("Value after Pre-increment : " + number);
-        int incre = ++number;
-        return incre;
 
     }
-    public static int decrement(int number){
-       System.out.println("Value after Pre-decrement : " + number);
-        int decre = --number;
-        return decre;
+    public static int preIncrement(int number){
+
+        int preResult = ++number;
+        return preResult;
+    }
+    public static int postIncrement(int number){
+
+        int postResult = number++;
+        return postResult;
+    }
+
+    public static int preDecrement(int number){
+
+        int preDecResult = --number;
+        return preDecResult;
+
+    }
+    public static int postDecrement(int number){
+
+        int postDecResult = number--;
+        return postDecResult;
 
     }
 
